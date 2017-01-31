9535c795638bd0a4586cf1211c28732020301373
Ian Whitney <whit0694@umn.edu>
2017-01-30 20:42:53 -0600

    Remove duplicated test
    When I first ported this test to RSpec my local copy was way out of
    date. The test had already been ported to Rspec in master.
    
    I'm deleting my duplicate test and updating what we use for the
    'request_double' in the remaining tests, using a verifiable
    double instead of a generic one.

--- spec/lib/umn_shib_auth/controller_methods_spec.rb
+++ spec/lib/umn_shib_auth/controller_methods_spec.rb
@@ -1,6 +1,8 @@
 require_relative "../../spec_helper"
 
 RSpec.describe UmnShibAuth::ControllerMethods do
+  let(:request_double) { instance_double(ActionDispatch::Request) }
+
   describe "A proxied shib app" do
     let(:eppn_var)    { 'HTTP_EPPN' }
     let(:internet_id) { 'asdf' }
@@ -9,7 +11,7 @@ RSpec.describe UmnShibAuth::ControllerMethods do
 
     before do
       UmnShibAuth.eppn_variable = eppn_var
-      request_double = double('request_double', env: {eppn_var => eppn})
+      allow(request_double).to receive(:env).and_return(eppn_var => eppn)
       allow(controller).to receive(:request).and_return(request_double)
     end
 
@@ -21,10 +23,10 @@ RSpec.describe UmnShibAuth::ControllerMethods do
 
   describe "shib_logout_url" do
     let(:controller)  { DummyController.new }
-    let(:host)        { "example.com"}
+    let(:host)        { "example.com" }
 
     before do
-      request_double = double('request_double', env: {"Shib-Identity-Provider" => shib_identity_provider})
+      allow(request_double).to receive(:env).and_return("Shib-Identity-Provider" => shib_identity_provider)
       allow(controller).to receive(:request).and_return(request_double)
       allow(request_double).to receive(:host).and_return(host)
     end
@@ -33,14 +35,14 @@ RSpec.describe UmnShibAuth::ControllerMethods do
       let(:shib_identity_provider) { "login-test.umn.edu" }
 
       it "returns encoded test logout URL" do
-        expect(controller.shib_logout_url).to eq "https://#{host}/Shibboleth.sso/Logout?return=https%3A%2F%2F#{ shib_identity_provider }%2Fidp%2Fprofile%2FLogout"
+        expect(controller.shib_logout_url).to eq "https://#{host}/Shibboleth.sso/Logout?return=https%3A%2F%2F#{shib_identity_provider}%2Fidp%2Fprofile%2FLogout"
       end
     end
     context "production IDP" do
       let(:shib_identity_provider) { "login.umn.edu" }
 
       it "returns encoded production logout URL" do
-        expect(controller.shib_logout_url).to eq "https://#{host}/Shibboleth.sso/Logout?return=https%3A%2F%2F#{ shib_identity_provider }%2Fidp%2Fprofile%2FLogout"
+        expect(controller.shib_logout_url).to eq "https://#{host}/Shibboleth.sso/Logout?return=https%3A%2F%2F#{shib_identity_provider}%2Fidp%2Fprofile%2FLogout"
       end
     end
   end
